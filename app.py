693dd18d9f5f311ecffa4d521aec500cc2304f84
Duncan Bain <zyrislp@gmail.com>
2022-11-21 18:37:02 -0700

    Added rest of GUI and File I/O

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import ttk
 class Navbar(tk.Frame): ...
 class Toolbar(tk.Frame): ...
 class Statusbar(tk.Frame): ...
@@ -27,6 +27,21 @@ class Main(tk.Frame):
         
         
         # Middle Row
+        choices = ["Extav", "Cyxie"] #array of player names to select from Comboboxes. This should be blank normally.
+        # P1 Listbox of names
+        self.lb1 = ttk.Combobox(self, values = choices)
+        self.lb1.grid(row=1, column=0, columnspan=2)
+        
+        # Entry box for the current tournament phase. (Winners R1, Losers R2, Winners-Final...)
+        self.phase = tk.StringVar()
+        tk.Entry(self, textvariable=self.phase).grid(row=1, column=2)
+        
+        self.lb2 = ttk.Combobox(self, values = choices)
+        self.lb2.grid(row=1, column=3, columnspan=2)
+        
+        #Bottom Row
+        self.btn_update = tk.Button(self, text="Update Files", command=self.update_files)
+        self.btn_update.grid(row=2, column=1, columnspan=3)
         
     
     def switch_players(self):
@@ -44,15 +59,26 @@ class Main(tk.Frame):
         self.p2_score.set(p1s)
         
         #Logging to console the action for now
-        print("Swapped player names & scores")   
-
-
+        print("Swapped player names & scores")
+        
+    def update_files(self):
+        files = {
+            "p1name.txt": self.p1_name.get(),
+            "p2name.txt": self.p1_name.get(),
+            "p1score.txt": str(self.p1_score.get()),
+            "p2score.txt": str(self.p2_score.get()),
+            "phase.txt": self.phase.get()
+        }
+        for e in files:
+            f = open(e, "w")
+            f.write(files[e])
+            f.close()
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-
+        
         self.statusbar = Statusbar(self)
         self.toolbar = Toolbar(self)
         self.navbar = Navbar(self)
@@ -65,5 +91,6 @@ class MainApplication(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title("ProSeed")
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
